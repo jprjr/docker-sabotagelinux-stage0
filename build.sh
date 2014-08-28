10ad6696f289b11ca2e16ce419315dd32e8f6951
John Regan <john@jrjrtech.com>
2014-08-28 16:18:11 -0500

    changing build to make more sense

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ today=$(date +"%Y%m%d")
 git filter-branch -f --tree-filter 'rm -rf output/sabotage-stage0*' --prune-empty master
 
 docker run -v $(pwd)/output:/output -v $(pwd)/script:/opt/mkimage jprjr/arch /opt/mkimage/mkimage-sabotage.sh $today
+git add output/sabotage-stage0-$today.tar.xz && git commit -m "Updating rootfs $today"
 
 cp dockersrc/Dockerfile output/Dockerfile
 sed -i "s/##DATE##/$today/" output/Dockerfile
-
 git add output/Dockerfile && git commit -m "Updating dockerfile $today"
-git add output/sabotage-stage0-$today.tar.xz && git commit -m "Updating rootfs $today"
